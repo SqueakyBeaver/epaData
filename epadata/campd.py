d2f32653c1932ddeae0f39533368bec38d56019b
Beaverr <squeaky.beaver4133@protonmail.com>
2026-09-11 12:23:41 -0500

    Initial campd api stuff; basic search filter page thingy (quickpush)

--- epadata/campd.py
+++ epadata/campd.py
@@ -1,6 +1,6 @@
 from os import getenv
-from typing import Any
 
+import pandas as pd
 import requests
 from dotenv import load_dotenv
 
@@ -8,50 +8,96 @@ from dotenv import load_dotenv
 class CAMPDClient:
     def __init__(self):
         load_dotenv()
-        self.api_key = getenv("CAMPD_API_KEY")
+        self.api_key = str(getenv("CAMPD_API_KEY"))
+
+        # TODO: Save some of these requests (like fuel codes) in a SQLite database so we don't have to request every time
+
+        if not self.api_key:
+            raise ValueError("No CAMPD_API_KEY found in environment variables or .env")
 
     def send_request(
-        self, endpoint: str, _headers: dict[str, Any] | None = None, **params
-    ):
+        self,
+        endpoint: str,
+        *,
+        _headers: dict[str, str | bytes] | None = None,
+        **params,
+    ) -> list[dict[str, str]]:
         if not "api_key" in params:
             params["api_key"] = self.api_key
 
-        headers = {"x-api-key": self.api_key}
+        headers: dict[str, str | bytes] = {"x-api-key": self.api_key}
         if _headers:
             headers.update(_headers)
 
         resp = requests.get(
-            url=f"https://api.epa.gov/easey/{endpoint}", headers=headers, params=params
+            url=f"https://api.epa.gov/easey/{endpoint}",
+            headers=headers,
+            params=params,
+            stream=True,
         )
 
-        return resp
+        return resp.json()["items"]
 
-    # TODO: Add different functions for requesting facilities and stuff idk
-    
     def get_state_codes(self) -> list[dict[str, str]]:
         """
-        Get state codes that CAMPD uses. Returned format is an array of:
+        Get state codes that CAMPD uses.
+        Returned format is an array of:
         { "stateCode": "XX", "stateName": "full state name", "epaRegion": "##" }
         """
-        resp = self.send_request(
+        return self.send_request(
             endpoint="master-data-mgmt/state-codes",
         )
 
-        return resp.json()["items"]
-
     def get_fuel_type_codes(self) -> list[dict[str, str]]:
         """
-        Get fuel type codes that CAMPD uses. Returned format is an array of:
+        Get fuel type codes that CAMPD uses.
+        Returned format is an array of:
         { "fuelTypeCode": "XX", "fuelTypeDescription": "Fuel name",
           "fuelGroupCode": "XXXXXXXX", "fuelGroupDescription": "Fuel Group Name" }
         """
-        resp = self.send_request(endpoint="master-data-mgmt/fuel-type-codes")
+        return self.send_request(endpoint="master-data-mgmt/fuel-type-codes")
 
-        return resp.json()["items"]
+    def get_facilities(self) -> list[dict[str, str]]:
+        """
+        Get facility names and codes that CAMPD has data on.
+        Returned format is an array of:
+        { 'facilityRecordId': ####, 'facilityId': ####,
+          'facilityName': 'facility name', 'stateCode': 'XX' }
+        """
+        return self.send_request(endpoint="facilities-mgmt/facilities")
+
+    def _get_facility_attributes_for_year(self, year: str | tuple[str]):
+        """
+        Get applicable facility attributes for the given year(s).
+        If `year` is a tuple, it should be in the format [start, stop],
+          and the attributes for all years from start to stop (inclusive) will be fetched
+        If a facility does not have data for that time, it is not included.
+        NOTE: This returns A LOT of data.
+        """
+        if isinstance(year, tuple):
+            year = "|".join(str(i) for i in range(int(year[0]), int(year[1]) + 1))
+
+        res = self.send_request(
+            endpoint="facilities-mgmt/facilities/attributes/applicable",
+            year=year,
+        )
+        return res
+
+    def get_facilities_for_year(self, year: str | tuple[str]) -> pd.DataFrame:
+        """
+        Get a list of facility codes and names that have data in a given year
+        """
+        attrs = pd.json_normalize(self._get_facility_attributes_for_year(year))
+        facilities = pd.json_normalize(self.get_facilities())
+
+        return facilities.loc[
+            facilities["facilityId"].isin(attrs["facilityId"].unique()),
+            ["facilityId", "facilityName"],
+        ]
 
 
 if __name__ == "__main__":
     c = CAMPDClient()
 
-    codes = c.get_fuel_type_codes()
+    codes = c.get_facilities_for_year(("2020", "2022"))
     print(codes)
--- epadata/routes/explorer.py
+++ epadata/routes/explorer.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 from epadata.campd import CAMPDClient
 
@@ -9,4 +9,20 @@ bp = Blueprint("explore", __name__, url_prefix="/explore")
 def campd():
     client = CAMPDClient()
 
-    return render_template("search_campd.html", state_codes=client.get_state_codes())
+    return render_template(
+        "search_campd.html",
+        state_codes=client.get_state_codes(),
+        fuel_type_codes=client.get_fuel_type_codes(),
+    )
+
+
+@bp.post("/campd/submit_years")
+def submit_years():
+    client = CAMPDClient()
+
+    print(request.args)
+
+    return render_template(
+        "fragments/search/filters_for_year.html",
+        facility_codes=client.get_facilities_for_year("2022"),
+    )
